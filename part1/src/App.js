bee9b75ab3b1a64637339e09ddd8b19afbb5186c
avnms <sarmaakondivnm@icloud.com>
2023-06-20 19:42:19 +0530

    How to render Objects correctly

--- part1/src/App.js
+++ part1/src/App.js
@@ -13,11 +13,25 @@ const App = () => {
   const name = "Peter";
   const age = 10;
 
+  const friends = [
+    { name: "John", age: 4 },
+    { name: "Daisy", age: 10 },
+  ];
+
+  const neighbours = ["Sam", "Pam"];
+
   return (
     <>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
+      <p>
+        {friends[0].name} {friends[0].age}
+      </p>
+      <p>
+        {friends[1].name} {friends[1].age}
+      </p>
+      <p>{neighbours}</p>
     </>
   );
 };
